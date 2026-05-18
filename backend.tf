99e9f2307e26dbd07c8e938dbce412e653c82810
Victoria Ifeyinwa <okorovictoriaifeyinwa@gmail.com>
2026-05-18 02:02:31 +0100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "my-tf-state-backend-22becd28"
+    bucket = "my-tf-state-backend-22becd28-ifynwa"
     key    = "prodution/terraform.tfstate"
     region = "us-east-1"
   }
