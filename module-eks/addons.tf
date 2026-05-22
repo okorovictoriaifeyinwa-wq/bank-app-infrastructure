1a5dace777cd381579fc8eb9ae7ff9e35b60b9a2
Victoria Ifeyinwa <okorovictoriaifeyinwa@gmail.com>
2026-05-22 17:38:46 +0100

    fix: cooment out helm releases for destroy
    Comment out Helm and Kubernetes provider configurations and related resources.

--- module-eks/addons.tf
+++ module-eks/addons.tf
@@ -1,63 +1,51 @@
-provider "helm" {
-    kubernetes {
-        host                   = aws_eks_cluster.eks.endpoint
-        cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority[0].data)
-        token                  = data.aws_eks_cluster_auth.eks.token
-    }
-}
+# Helm releases temporarily disabled
+# Uncomment when EKS cluster is running
 
-provider "kubernetes" {
-  alias                  = "eks"
-  host                   = aws_eks_cluster.eks.endpoint
-  cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.eks.token
-}
+# provider "helm" {}
+# provider "kubernetes" {}
 
-data "aws_eks_cluster_auth" "eks" {
-    name = aws_eks_cluster.eks.name
-}
-resource "helm_release" "nginx_ingress" {
-    name       = "nginx-ingress"
-    repository = "https://kubernetes.github.io/ingress-nginx"
-    chart      = "ingress-nginx"
-    version    = "4.12.0"
-    namespace  = "ingress-nginx"
-    create_namespace = true
+# data "aws_eks_cluster" "eks" {}
+# data "aws_eks_cluster_auth" "eks" {}
 
-    values = [file("${path.module}/nginx-ingress-values.yaml")]
-    depends_on = [ aws_eks_node_group.eks_node_group ]
-}
+# resource "helm_release" "nginx_ingress" {
+#   name             = "nginx-ingress"
+#   repository       = "https://kubernetes.github.io/ingress-nginx"
+#   chart            = "ingress-nginx"
+#   version          = "4.8.0"
+#   namespace        = "ingress-nginx"
+#   create_namespace = true
+#   values           = [file("${path.module}/nginx-ingress-values.yaml")]
+#   depends_on       = [var.eks_cluster_eks_node_group]
+# }
 
-data "aws_lb" "nginx_ingress" {
-  tags = {
-    "kubernetes.io/service-name" = "ingress-nginx/nginx-ingress-ingress-nginx-controller"
-  }
+# data "aws_lb" "nginx_ingress" {
+#   tags = {
+#     "kubernetes.io/ingress-nginx/ingress-nginx-nginx-controller" = "owned"
+#   }
+#   depends_on = [helm_release.nginx_ingress]
+# }
 
-  depends_on = [helm_release.nginx_ingress]
-}
+# resource "helm_release" "cert_manager" {
+#   name             = "cert-manager"
+#   repository       = "https://charts.jetstack.io"
+#   chart            = "cert-manager"
+#   version          = "1.14.4"
+#   namespace        = "cert-manager"
+#   create_namespace = true
+#   set {
+#     name  = "installCRDs"
+#     value = "true"
+#   }
+#   depends_on = [helm_release.nginx_ingress]
+# }
 
-resource "helm_release" "cert_manager" {
-    name       = "cert-manager"
-    repository = "https://charts.jetstack.io"
-    chart      = "cert-manager"
-    version    = "1.14.5"
-    namespace  = "cert-manager"
-    create_namespace = true
-    set {
-        name  = "installCRDs"
-        value = "true"
-    }
-    depends_on = [ helm_release.nginx_ingress ]
-}
-#==================================================
-
-resource "helm_release" "argocd" {
-    name             = "argocd"
-    repository       = "https://argoproj.github.io/argo-helm"
-    chart            = "argo-cd"
-    version          = "5.51.6"
-    namespace        = "argocd"
-    create_namespace = true
-    values = [file("${path.module}/argocd-values.yaml")]
-    depends_on = [ helm_release.nginx_ingress, helm_release.cert_manager]
-}
+# resource "helm_release" "argocd" {
+#   name             = "argocd"
+#   repository       = "https://argoproj.github.io/argo-helm"
+#   chart            = "argo-cd"
+#   version          = "5.51.6"
+#   namespace        = "argocd"
+#   create_namespace = true
+#   values           = [file("${path.module}/argocd-values.yaml")]
+#   depends_on       = [helm_release.nginx_ingress, helm_release.cert_manager]
+# }
