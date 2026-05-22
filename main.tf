151f50849dc0e6bd775a2501c05822d622df66f6
Victoria Ifeyinwa <okorovictoriaifeyinwa@gmail.com>
2026-05-22 18:05:48 +0100

    Comment out nginx load balancer variables
    Comment out nginx load balancer variables in dns-deployment module

--- main.tf
+++ main.tf
@@ -37,9 +37,9 @@ module "dns-deployment" {
     source = "./module-dns"
     environment = var.environment
     domain_name = var.domain_name
-    nginx_lb_ip = module.eks-deployment.nginx_lb_ip
-    nginx_ingress_load_balancer_hostname = module.eks-deployment.nginx_ingress_load_balancer_hostname
-    nginx_ingress_lb_dns = module.eks-deployment.nginx_ingress_lb_dns
+#    nginx_lb_ip = module.eks-deployment.nginx_lb_ip
+#    nginx_ingress_load_balancer_hostname = module.eks-deployment.nginx_ingress_load_balancer_hostname
+#    nginx_ingress_lb_dns = module.eks-deployment.nginx_ingress_lb_dns
   
 }
 
