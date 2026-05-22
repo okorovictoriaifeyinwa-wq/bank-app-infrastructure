427e5c50be7f05d3cb5a895c1ddc680ebea9ae08
Victoria Ifeyinwa <okorovictoriaifeyinwa@gmail.com>
2026-05-22 18:17:17 +0100

    Update DNS module with new load balancer settings

--- main.tf
+++ main.tf
@@ -34,12 +34,12 @@ module "eks-deployment" {
 }
 
 module "dns-deployment" {
-    source = "./module-dns"
-    environment = var.environment
-    domain_name = var.domain_name
-#    nginx_lb_ip = module.eks-deployment.nginx_lb_ip
-#    nginx_ingress_load_balancer_hostname = module.eks-deployment.nginx_ingress_load_balancer_hostname
-#    nginx_ingress_lb_dns = module.eks-deployment.nginx_ingress_lb_dns
+  source      = "./module-dns"
+  environment = var.environment
+  domain_name = var.domain_name
+  nginx_lb_ip = "0.0.0.0"
+  nginx_ingress_load_balancer_hostname = "placeholder.example.com"
+  nginx_ingress_lb_dns = "placeholder.example.com"
   
 }
 
